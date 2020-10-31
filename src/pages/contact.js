b71d592bd14fea3ba50fa00ada543952f915f5f8
Bogodar Shmyglyk <bogodar@gmail.com>
2020-10-31 23:06:27 +0200

    chack

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ import "../css/custom.css"
 function contact() {
   return (
     <section id="contact" className="contact-body">
-      <h4 className="pt5-l pt0 pt5-m  p10 tc f3 ml3">Contact</h4>
+      <h4 className="pt5-l pt0 pt5-m  p10 tc f3 ml3">Contact me</h4>
       <div className="contact__body flex justify-between mt5-l mt2 items-center flex-wrap-m ">
         <Icon style={{ width: "450px", height: "400px", margin: "0 auto" }} />
         <div className="center">
